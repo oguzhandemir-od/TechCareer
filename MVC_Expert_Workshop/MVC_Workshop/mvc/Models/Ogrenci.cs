659629ce18d3c9d7490dcdbbc38febbbbea33260
oguzhandemir-od <141906081+oguzhandemir-od@users.noreply.github.com>
2024-10-03 22:56:15 +0300

    Student

---
+++ MVC_Expert_Workshop/MVC_Workshop/mvc/Models/Ogrenci.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mvc.Models
+{
+    public class Ogrenci
+    {
+        public int Id {  get; set; }
+        public string Ad {  get; set; }
+        public string Soyad { get; set; }
+        public string TCKimlikNo { get; set; }
+        public string BolumAd { get; set; }
+        public int GirisYili {  get; set; }
+
+    }
+}
