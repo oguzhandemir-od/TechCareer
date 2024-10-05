90f1aa6615f16f374758f9a6d39e5abc8f9c058f
oguzhandemir-od <141906081+oguzhandemir-od@users.noreply.github.com>
2024-10-05 21:19:52 +0300

    Min - Max Length, TR Identity Number

--- MVC_Expert_Workshop/MVC_Workshop/mvc/Models/Ogrenci.cs
+++ MVC_Expert_Workshop/MVC_Workshop/mvc/Models/Ogrenci.cs
@@ -17,9 +17,14 @@ namespace mvc.Models
 
         [Required(ErrorMessage ="TC Kimlik Numarası Gereklidir!")]
         [DisplayName("Türkiye Cumhuriyeti Kimlik Numarası")]
+        [MinLength(11,ErrorMessage ="TC Kimlik No 11 haneli olmalıdır!")]
+        [MaxLength(11, ErrorMessage = "TC Kimlik No 11 haneli olmalıdır!")]
         public string TCKimlikNo { get; set; }
         public string BolumAd { get; set; }
         public int GirisYili {  get; set; }
+
+        //[EmailAddress(ErrorMessage ="Lütfen eposta adresinizi doğru giriniz!")]
+        //public string EPosta { get; set; }
 
     }
 }
