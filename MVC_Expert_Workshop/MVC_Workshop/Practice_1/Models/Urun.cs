df18f6342e743d1f5ce03bca5e0bebf4297c1303
oguzhandemir-od <141906081+oguzhandemir-od@users.noreply.github.com>
2024-10-05 11:03:50 +0300

    Start

---
+++ MVC_Expert_Workshop/MVC_Workshop/Practice_1/Models/Urun.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Practice_1.Models
+{
+    public class Urun
+    {
+        public int Id { get; set; }
+        public string UrunAdi { get; set; }
+        public string UrunMagazasi { get; set; }
+        public string UrunOzellikleri { get; set; }
+        public int UrunFiyati { get; set; }
+        public int UrunKargoSuresi { get; set; }
+
+    }
+}
