0da844c1047cb38cf5e3d9f8300f9678c6c98001
oguzhandemir-od <141906081+oguzhandemir-od@users.noreply.github.com>
2024-10-05 11:05:21 +0300

    Product Controller

---
+++ MVC_Expert_Workshop/MVC_Workshop/Practice_1/Controllers/UrunController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Practice_1.Controllers
+{
+    public class UrunController : Controller
+    {
+        // GET: Urun
+        public ActionResult Listele()
+        {
+            return View();
+        }
+    }
+}
