b21303c07bd55ed5f0c67055d36bd609c4b76e61
oguzhandemir-od <141906081+oguzhandemir-od@users.noreply.github.com>
2024-10-05 21:04:43 +0300

    Validators, TR Identity Required

--- MVC_Expert_Workshop/MVC_Workshop/mvc/Models/Ogrenci.cs
+++ MVC_Expert_Workshop/MVC_Workshop/mvc/Models/Ogrenci.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -10,6 +12,8 @@ namespace mvc.Models
         public int Id {  get; set; }
         public string Ad {  get; set; }
         public string Soyad { get; set; }
+
+        [Required(ErrorMessage ="TC Kimlik Numarası Gereklidir!")]
         public string TCKimlikNo { get; set; }
         public string BolumAd { get; set; }
         public int GirisYili {  get; set; }
