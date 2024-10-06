3a726e7209cfdd7c663e15f924e8da0ba4224a03
oguzhandemir-od <141906081+oguzhandemir-od@users.noreply.github.com>
2024-10-06 20:57:39 +0300

    Data Access

---
+++ MVC_Expert_Workshop/MVC_Workshop/efp/VeriErisimi/SatisContext.cs
@@ -0,0 +1,21 @@
+﻿using efp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace efp.VeriErisimi
+{
+    public class SatisContext : DbContext
+    {
+        public SatisContext() : base("SatisVeriTabani") { }
+        public DbSet<Urun> Urunler { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
