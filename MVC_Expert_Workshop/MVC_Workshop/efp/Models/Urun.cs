f6213fd7f7ad8796f8c0068983a6e47ceeb40d99
oguzhandemir-od <141906081+oguzhandemir-od@users.noreply.github.com>
2024-10-06 11:00:01 +0300

    Product Model

---
+++ MVC_Expert_Workshop/MVC_Workshop/efp/Models/Urun.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace efp.Models
+{
+    public class Urun
+    {
+        public int Id { get; set; }
+        public string Ad {  get; set; }
+        public double Fiyat { get; set; }
+        public int StokSayisi { get; set; }
+
+    }
+}
