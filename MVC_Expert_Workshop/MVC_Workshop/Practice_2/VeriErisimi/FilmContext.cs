df3f44819118e5aec0896f1146fe0e9f67b3bf5f
oguzhandemir-od <141906081+oguzhandemir-od@users.noreply.github.com>
2024-10-07 10:20:03 +0300

    Movie Context

---
+++ MVC_Expert_Workshop/MVC_Workshop/Practice_2/VeriErisimi/FilmContext.cs
@@ -0,0 +1,22 @@
+﻿using Practice_2.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace Practice_2.VeriErisimi
+{
+    public class FilmContext:DbContext
+    {
+        public FilmContext() : base("FilmVeriTabani") { }
+
+        public DbSet<Film> Filmler { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
