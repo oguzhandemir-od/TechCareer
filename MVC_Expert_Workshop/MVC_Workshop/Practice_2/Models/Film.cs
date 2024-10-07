c7a518f32c975e3edd396f008db0baf9c5126423
oguzhandemir-od <141906081+oguzhandemir-od@users.noreply.github.com>
2024-10-07 10:19:56 +0300

    Movie Model

---
+++ MVC_Expert_Workshop/MVC_Workshop/Practice_2/Models/Film.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Practice_2.Models
+{
+    public class Film
+    {
+        public int Id {  get; set; }
+        public string Ad {  get; set; }
+        public string Tur { get; set; }
+        public string Aciklama { get; set; }
+        public double Puan {  get; set; }
+
+    }
+}
