5dea5adf40d60cf8a76551341d892d87327d0494
oguzhandemir-od <141906081+oguzhandemir-od@users.noreply.github.com>
2024-10-08 08:41:21 +0300

    Seed Function for Inital Data

---
+++ MVC_Expert_Workshop/MVC_Workshop/efp/VeriErisimi/ilksatisInitializer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace efp.VeriErisimi
+{
+    public class ilksatisInitializer:DropCreateDatabaseIfModelChanges<SatisContext>
+    {
+        protected override void Seed(SatisContext context)
+        {
+
+            base.Seed(context);
+        }
+    }
+}
